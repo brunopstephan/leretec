f166ed9e9f00f42991319e9e04a1d7798781d20e
Lab3-15 <Heclypso@gmail.com>
2022-08-01 14:28:53 -0300

    Inicio da tela de historia

--- resources/views/leretec/history.blade.php
+++ resources/views/leretec/history.blade.php
@@ -2,6 +2,21 @@
 
 @section('container')
     <div class="container card-container">
-        USAR MESMA LOGICA DO EDIT/UPDATE
+        <div class="card_info">
+            <div class="img_historia">
+                <img src="https://static-cse.canva.com/blob/759754/IMAGE1.jpg" alt="Avatar" style="width:30%">
+            </div>
+            <div class="info">
+                <h1>Titulo da Historia</h1>
+                <p>Nome Aluno</p>
+                <p>Data</p>
+                <p>Curso/Serie</p>
+                <p>Sinopse</p>
+            </div>
+          </div>
+          <div class="btn_info">
+            <a class="btn-all" href="">Ler Historia</a>
+            <a class="btn-all" href="">Baixar Historia</a>
+          </div>
     </div>
 @endsection
