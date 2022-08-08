391fdfce0e80bcd6580b859fd83bfdb8535c7f13
Heclypso <heclypso@gmail.com>
2022-08-07 23:15:20 -0300

    finalização da tela de historia

--- resources/views/leretec/history.blade.php
+++ resources/views/leretec/history.blade.php
@@ -4,19 +4,17 @@
     <div class="container card-container">
         <div class="card_info">
             <div class="img_historia">
-                <img src="https://static-cse.canva.com/blob/759754/IMAGE1.jpg" alt="Avatar" style="width:30%">
+                <img src="https://pbs.twimg.com/profile_images/1202532098562830341/e0-Zn6De_400x400.jpg" alt="Avatar">
             </div>
-            <div class="info">
+            <div class="info_historia">
+                <p>Nome Aluno - Data - Curso - Ano</p>
                 <h1>Titulo da Historia</h1>
-                <p>Nome Aluno</p>
-                <p>Data</p>
-                <p>Curso/Serie</p>
                 <p>Sinopse</p>
+                <div class="btn_info">
+            <a class="btn-all btn-info" href="">Ler Historia</a>
+            <a class="btn-all btn-info" href="">Baixar Historia</a>
+          </div>
             </div>
-          </div>
-          <div class="btn_info">
-            <a class="btn-all" href="">Ler Historia</a>
-            <a class="btn-all" href="">Baixar Historia</a>
-          </div>
+        </div>
     </div>
 @endsection
